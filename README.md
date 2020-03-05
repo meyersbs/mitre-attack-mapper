20eaad8bb4fe94943c74018b05e04d4b6fbd9021
Benjamin S. Meyers <meyers.bs@gmail.com>
2020-03-05 09:55:03 -0500

    Refactorings.

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # mitre-attack-mapper
-Classification model to map Splunk logs to MITRE ATT&amp;CK States
+Scripts for training and testing classifiers that map Splunk logs to MITRE ATT&amp;CK tactics and techniques.
+
+## Dependencies
+
+Run `sudo pip3 install -r requirements.txt` to install the required Python libraries.
+
+## Usage
+
+### Info
+
+Running `./main.py info` will read in the data from `/data/CPTC2018.csv` and print out some useful information about the dataset.
+
+Sample Output:
+```
+Read 261 data points from '/home/benjamin/Code/mitre-attack-mapper/data/CPTC2018.csv'.
+DATASET INFO:
+  Data Points: 261
+  TACTICS:
+    'Discovery': 116
+    ...
+    'Credential Access, Lateral Movement, Lateral Movement': 23
+    'Pre-Attack': 25
+    ...
+  TECHNIQUES:
+    'Network Service Scanning': 105
+    ...
+    'Pre-Attack': 27
+    'Brute Force, Valid Accounts, Remote Services': 26
+    ...
+```
+
+### Train
+
+Running `./main.py train <model_type> <target_label>` will train a simple bag-of-words model on the event data, and then run cross validation on many combinations of model parameters. The best model parameters and the classification report (precision, recall, F1-score, etc.) will be printed out.
+
+Currently implemented model types are: `nb` ((naive bayes)[https://scikit-learn.org/stable/modules/generated/sklearn.naive\_bayes.MultinomialNB.html]), `linear` ((various linear models)[https://scikit-learn.org/stable/modules/generated/sklearn.linear\_model.SGDClassifier.html]). `nb` takes about 2 minutes to run. `linear` takes significantly longer -- I'll be refactoring to fix that.
+
+Valid target labels are: `tactics`, `techniques`.
+
+Sample Output:
+```
+Best Score for MultinomialNB:
+    0.6558823529411766
+Best Parameters for MultinomialNB:
+    clf__alpha: 0.01
+    clf__fit_prior: True
+    tfidf__use_idf: True
+    vect__ngram_range: (1, 2)
+Best Model Accuracy: 0.632183908045977
+                                                       precision    recall  f1-score   support
+
+         Brute Force, Valid Accounts, Remote Services       0.00      0.00      0.00         1
+                                           Collection       0.00      0.00      0.00         1
+                  Credential Access, Lateral Movement       0.44      0.44      0.44         9
+Credential Access, Lateral Movement, Lateral Movement       0.78      0.70      0.74        10
+                                            Discovery       0.85      0.83      0.84        35
+                         Discovery, Credential Access       0.00      0.00      0.00         1
+                                 Discovery, Discovery       0.00      0.00      0.00         4
+                                            Execution       0.00      0.00      0.00         1
+                          Execution, Lateral Movement       0.25      1.00      0.40         1
+        Execution, Lateral Movement, Lateral Movement       0.00      0.00      0.00         1
+                                       Initial Access       1.00      0.50      0.67         2
+                                     Lateral Movement       0.29      0.40      0.33         5
+                   Lateral Movement, Lateral Movement       0.40      1.00      0.57         2
+                        Lateral Movement, Persistence       0.00      0.00      0.00         1
+                Lateral Movement, Privlege Escalation       0.00      0.00      0.00         1
+                                          Persistence       0.00      0.00      0.00         1
+                                           Pre-Attack       0.60      0.90      0.72        10
+                                 Privilege Escalation       0.00      0.00      0.00         1
+
+                                             accuracy                           0.63        87
+                                            macro avg       0.26      0.32      0.26        87
+                                         weighted avg       0.60      0.63      0.60        87
+```
---
+++ main.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+
+#### PYTHON IMPORTS ############################################################
+import csv
+import numpy as NP
+import os
+import sys
+import warnings
+
+from collections import Counter
+
+
+#### SCIKITLEARN IMPORTS #######################################################
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+#### GLOBALS ###################################################################
+CURR_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join(CURR_PATH, "data")
+DATA_FILE = os.path.join(DATA_PATH, "CPTC2018.csv")
+
+
+#### HELPER FUNCTIONS ##########################################################
+def readData(filename):
+    """
+    Read data from a CSV file. The CSV file should have the following 4 columns:
+      1 ID          Unique ID for event
+      2 EVENT       Raw text of a Splunk event (log)
+      3 TACTIC      One or more MITRE ATT&CK Tactics
+      4 TECHNIQUE   One or more MITRE ATT&CK Techniques
+
+    GIVEN:
+      filename (str)    file path to read data from
+
+    RETURN:
+      data (list)       list of events from CSV file
+      labels (dict)     dictionary with two keys, "tactics" and "techniques";
+                        each value is a list of human-annotated labels
+    """
+    data = list()
+    labels = {"tactics": list(), "techniques": list()}
+    with open(filename, newline="") as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=",", quotechar="\"")
+
+        # Skip header row
+        next(csv_reader)
+
+        # Read data from CSV
+        for row in csv_reader:
+            data.append(row[1])
+            labels["tactics"].append(row[2])
+            labels["techniques"].append(row[3])
+
+    print("Read {} data points from '{}'.".format(len(data), filename))
+    return data, labels
+
+
+def printStats(data, labels):
+    """ Print useful information about the data. """
+    cnt_tactics = Counter(labels["tactics"])
+    k_tactics = list(cnt_tactics.keys())
+    v_tactics = list(cnt_tactics.values())
+    cnt_techniques = Counter(labels["techniques"])
+    k_techniques = list(cnt_techniques.keys())
+    v_techniques = list(cnt_techniques.values())
+
+    print("DATASET INFO:")
+    print("  Data Points: {}".format(len(data)))
+    print("  TACTICS:")
+    for i in range(0, len(k_tactics)):
+        print("    '{}': {}".format(k_tactics[i], v_tactics[i]))
+    print("  TECHNIQUES:")
+    for i in range(0, len(k_techniques)):
+        print("    '{}': {}".format(k_techniques[i], v_techniques[i]))
+
+
+def trainTestSplit(data, labels, target_class, test_size):
+    """
+    Split the data and it's labels into training and testing sets.
+
+    GIVEN:
+      data (list)           list of events
+      labels (dict)         human-annotated labels for data
+      target_class (str)    one of ["tactics", "techniques"]
+      test_size (double)    percentage to hold out for testing
+
+    RETURN:
+      X_train (list)    list of training data values
+      X_test (list)     list of testing data values
+      Y_train (list)    list of training data labels
+      Y_test (list)     list of testing data labels
+    """
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        data, labels[target_class], test_size=test_size,
+        #stratify=labels[target_class]
+    )
+
+    return X_train, X_test, Y_train, Y_test
+
+def trainMNB(data, labels, target_class, test_size):
+    """
+    Train and test the performance of MultinomialNB with various parameter
+    combinations.
+    """
+    X_train, X_test, Y_train, Y_test = trainTestSplit(
+        data, labels, target_class, test_size)
+
+    pipe_MNB = Pipeline([
+        ("vect", CountVectorizer()),
+        ("tfidf", TfidfTransformer()),
+        ("clf", MultinomialNB())
+    ])
+    
+    # All combinations of these parameters will be tested
+    GS_MNB_params = {
+        "vect__ngram_range": [(1, 1), (1, 2), (1, 3)], # Unigrams, Bigrams, Trigrams
+        "tfidf__use_idf": (True, False),
+        "clf__alpha": (0.01, 0.05, 0.1, 0.3, 0.5, 0.7, 0.9, 0.95, 0.99, 1),
+        "clf__fit_prior": (True, False)
+    }
+
+    # Test all combinations of GS_MNB_params using 10-fold CV and all available CPU's
+    pipe_GS_MNB = GridSearchCV(pipe_MNB, GS_MNB_params, cv=25, n_jobs=-1)
+    pipe_GS_MNB.fit(X_train, Y_train)
+
+    best_params = pipe_GS_MNB.best_params_
+    print("Best Score for MultinomialNB:")
+    print("    {}".format(pipe_GS_MNB.best_score_))
+    print("Best Parameters for MultinomialNB:")
+    for p in sorted(GS_MNB_params.keys()):
+        print("    {}: {}".format(p, best_params[p]))
+
+    #print(pipe_GS_MNB.cv_results_)
+
+    pipe_GS_MNB_predicted = pipe_GS_MNB.predict(X_test)
+    print(classification_report(Y_test, pipe_GS_MNB_predicted))
+
+
+def trainLinear(data, labels, target_class, test_size):
+    """
+    Train and test the performance of SGDClassifier (various linear models) with
+    various parameter combinations.
+    """
+    X_train, X_test, Y_train, Y_test = trainTestSplit(
+        data, labels, target_class, test_size)
+
+    pipe_SGD = Pipeline([
+        ("vect", CountVectorizer()),
+        ("tfidf", TfidfTransformer()),
+        ("clf", SGDClassifier())
+    ])
+
+    # All combinations of these parameters will be tested
+    GS_SGD_params = {
+        "vect__ngram_range": [(1, 1), (1, 2), (1, 3)],
+        "tfidf__use_idf": (True, False),
+        "clf__loss": ("hinge", "log", "modified_huber", "squared_hinge", "perceptron", "squared_loss", "huber", "epsilon_insensitive", "squared_epsilon_insensitive"),
+        "clf__l1_ratio": (0.01, 0.1, 0.5, 0.9, 0.99),
+        "clf__fit_intercept": (True, False),
+        "clf__alpha": (1e-2, 1e-3),
+        "clf__penalty": ("l1", "l2", "elasticnet"),
+        "clf__max_iter": (100, 500, 1000),
+        "clf__tol": (None, 1e-1, 1e-2, 1e-3),
+        "clf__eta0": (0.1, 0.5, 0.9, 1),
+        "clf__learning_rate": ("constant", "optimal", "invscaling", "adaptive")
+    }
+
+    # Test all combinations of GS_SGD_params using 10-fold CV and all available CPU's
+    pipe_GS_SGD = GridSearchCV(pipe_SGD, GS_SGD_params, cv=10, n_jobs=-1)
+    pipe_GS_SGD.fit(X_train, Y_train)
+
+    best_params = pipe_GS_SGD.best_params_
+    print("Best Score for SGDClassifer:")
+    print("    {}".format(pipe_GS_SGD.best_score_))
+    print("Best Parameters for SGDClassifier:")
+    for p in sorted(GS_SGD_params.keys()):
+        print("    {}: {}".format(p, best_params[p]))
+
+    pipe_GS_SGD_predicted = pipe_GS_SGD.predict(X_test)
+    print(classification_report(Y_test, peipe_GS_SGD_predicted))
+
+
+#### MAIN ######################################################################
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == "info":
+        data, labels = readData(DATA_FILE)
+        printStats(data, labels)
+    elif args[0] == "train":
+        data, labels = readData(DATA_FILE)
+        if args[1] == "nb":
+            trainMNB(data, labels, args[2], 0.33)
+        elif args[1] == "linear":
+            trainLinear(data, labels, args[2], 0.33)
+
